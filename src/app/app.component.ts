a6467b690cdbe1f267965d52dfe7fdb8b64c2ca2
Prem <mpremkumardpk@gmail.com>
2020-05-06 02:17:07 +0530

    added footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { FormControl, Validators } from '@angular/forms';
 import { ConfigService } from './services/config.service';
 import { StorageService } from './services/storage.service';
 import { ShuffleService } from './services/shuffle.service';
+import { StatisticsService } from './services/statistics.service';
 
 @Component({
   selector: 'app-root',
@@ -37,8 +38,9 @@ export class AppComponent {
   ];
 
   constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher,
-    private storage: StorageService,
-    private shuffle: ShuffleService,
+    public storage: StorageService,
+    public stat: StatisticsService,
+    public shuffle: ShuffleService,
     public config: ConfigService) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
